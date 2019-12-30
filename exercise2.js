f1d7db308e7fd173044400c12f51f15a84b1faa1
Tech2020 <59362189+Tech2020@users.noreply.github.com>
2019-12-30 04:49:33 -0700

    Create exercise2.js

---
+++ exercise2.js
@@ -0,0 +1,15 @@
+// add variable "firstName" and "lastName" // so that they equal your name
+var firstName = "Johnny";
+var lastName = "Bravo";
+
+// create a variable that holds the answer // of "firstName" + " " + "LastName"
+var fullName = firstName + " " + lastName;
+
+// Evaluate this question. What is a + b?
+var a = 34;
+var b = 21;
+a = 2;
+b + a // 23
+
+// What is c equal to?
+var c; // undefined
